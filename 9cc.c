a7dd2dcc8ac23ba8312c0950e1ce363fde2eefe0
Neccolini <shun11202991@gmail.com>
2020-12-11 15:12:53 +0900

    new

--- 9cc.c
+++ 9cc.c
@@ -10,8 +10,8 @@ typedef enum {
 	TK_NUM, //integer
 	TK_EOF, //End of File
 }TokenKind;
-typedef struct Token Token;
 
+typedef struct Token Token;
 struct Token {
 	TokenKind kind; //Token Type
 	Token *next; //next Token
@@ -19,6 +19,7 @@ struct Token {
 	char *str; // Token String
 };
 
+char *user_input;
 // Current Token
 Token *token;
 
@@ -30,6 +31,18 @@ void error(char *fmt, ...) {
 	fprintf(stderr, "\n");
 	exit(1);
 }
+void error_at(char *loc, char *fmt, ...) {
+	va_list ap;
+	va_start(ap,fmt);
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, " ");
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
 // If the next token is the expected symbol, 
 // go ahead of the token and return true.
 bool consume(char op) {
@@ -43,13 +56,13 @@ bool consume(char op) {
 // go ahead of the token and otherwise report the error.
 void expect(char op) {
 	if(token->kind != TK_RESERVED || token->str[0] != op)
-	       error("not '%c'",op);
+	       error_at(token->str, "expected '%c'", op);
 	token = token->next;
 }
 
 int expect_number() {
 	if(token->kind != TK_NUM)
-		error("not a number");
+		error_at(token->str, "expected a number");
 	int val = token->val;
 	token = token->next;
 	return val;
@@ -69,7 +82,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 }
 
 // tokenize p and return the result
-Token *tokenize(char *p) {
+Token *tokenize() {
+	char *p = user_input;
 	Token head;
 	head.next = NULL;
 	Token *cur = &head;
@@ -89,7 +103,7 @@ Token *tokenize(char *p) {
 			cur->val = strtol(p, &p ,10);
 			continue;
 		}
-		error("cannot tokenize the input");
+		error_at(p, "expected a number");
 	}
 	new_token(TK_EOF, cur, p);
 	return head.next;
@@ -103,8 +117,8 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	
-	token = tokenize(argv[1]);
-
+	user_input = argv[1];
+	token = tokenize();
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
